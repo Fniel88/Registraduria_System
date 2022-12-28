84fec3506adb2999c8ac9103e2f1d00e04f1d195
Fniel88 <117480530+Fniel88@users.noreply.github.com>
2022-12-27 19:23:02 -0500

    Delete repositories directory

--- repositories/candidateRepository.py
+++
@@ -1,9 +0,0 @@
-from models.candidate import Candidate
-from repositories.interface_repository import InterfaceRepository
-
-# Class where it is calling the interface repository and receive the class Candidate that after it is going to
-# call the AbstractModel
-
-
-class CandidateRepository(InterfaceRepository[Candidate]):
-    pass
--- repositories/tableRepository.py
+++
@@ -1,9 +0,0 @@
-from models.table import Table
-from repositories.interface_repository import InterfaceRepository
-
-# Class where it is calling the interface repository and receive the class Table that after it is going to
-# call the AbstractModel
-
-
-class TableRepository(InterfaceRepository[Table]):
-    pass
